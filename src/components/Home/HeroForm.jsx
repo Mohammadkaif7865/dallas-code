653bbbb5ab9cd5c9de3c636b3f30cc02240f2607
sib-vishal <vishal.maurya@sibinfo.tech>
2024-06-07 12:19:30 +0530

    commit

--- src/components/Home/HeroForm.jsx
+++ src/components/Home/HeroForm.jsx
@@ -1,188 +1,33 @@
-// import React, { useState } from "react";
-// import Services from "./Services";
-// import axios from "axios";
-// import "./heroform.css";
-// import { toast } from "react-toastify";
-
-// const HeroForm = ({ oneline }) => {
-//   const [formData, setFormData] = useState({
-//     name: "",
-//     phoneNo: "",
-//     companyName: "",
-//     websiteUrl: "",
-//     email: "",
-//     fromWhere: "dallas",
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prevData) => ({
-//       ...prevData,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     // console.log("Form Data:", formData);
-//     const data = {
-//       name: formData.name,
-//       company: formData.companyName,
-//       website: formData.websiteUrl,
-//       email: formData.email,
-//       phone: formData.phoneNo,
-//       fromWhere: formData.fromWhere,
-//     };
-
-//     // console.log(data);
-
-//     try {
-//       const API_TOKEN = "FgRCHG4OVv8Z1BcrjExKJcqspvTsUTCe";
-//       const url = "https://www.sibinfotech.com/api/send-email";
-//       const response = await fetch(url, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: API_TOKEN,
-//         },
-//         body: JSON.stringify(data),
-//       });
-
-//       console.log(response.data);
-//       if (response.status == 200) {
-//         toast.success("Email Send Successfully!");
-//         setFormData({
-//           name: "",
-//           companyName: "",
-//           websiteUrl: "",
-//           email: "",
-//           phoneNo: "",
-//         });
-
-//         setTimeout(() => {
-//           window.location.href = "https://sibinfotech.com/thanks";
-//         }, 2000);
-//       }
-//       if (!response.status == 200) {
-//         return toast.error("Somthing went wrong!");
-//       }
-//     } catch (error) {
-//       console.error("Error:", error);
-//     }
-//   };
-
-//   return (
-//     <>
-
-//       <div
-//         className={`w-[100%] bg-white rounded-2xl   ${
-//           oneline ? "py-12 " : "p-5"
-//         }`}
-//       >
-//         <h3 className="font-bold font-redhat text-[24px] md:text-[29px] text-center">
-//           Ready to get more leads
-//         </h3>
-//         <form
-//           className="Hero_form text-[#999] font-poppins"
-//           onSubmit={handleSubmit}
-//         >
-//           <div className={`grid   ${oneline ? "" : "grid-cols-2 gap-4 "}`}>
-//             <div class="user-box mt-[23px] ">
-//               <input
-//                 className=""
-//                 type="text"
-//                 name="name"
-//                 value={formData.name}
-//                 onChange={handleChange}
-//                 required
-//               />
-//               <label htmlFor="name" className="">
-//                 Name
-//               </label>
-//             </div>
-//             <div class="user-box mt-[23px] ">
-//               <input
-//                 className=""
-//                 type="text"
-//                 name="phoneNo"
-//                 value={formData.phoneNo}
-//                 onChange={handleChange}
-//                 required
-//               />
-//               <label htmlFor="phoneNo" className="">
-//                 Phone No
-//               </label>
-//             </div>
-//           </div>
-//           <div class="user-box mt-[23px] ">
-//             <input
-//               className=""
-//               type="text"
-//               name="companyName"
-//               value={formData.companyName}
-//               onChange={handleChange}
-//               required
-//             />
-//             <label htmlFor="companyName" className="">
-//               Company Name
-//             </label>
-//           </div>
-
-//           <div class="user-box mt-[23px] ">
-//             <input
-//               className=""
-//               type="text"
-//               name="websiteUrl"
-//               value={formData.websiteUrl}
-//               onChange={handleChange}
-//               required
-//             />
-//             <label htmlFor="websiteUrl" className="">
-//               Website Url
-//             </label>
-//           </div>
-
-//           <div class="user-box mt-[23px] ">
-//             <input
-//               className=""
-//               type="text"
-//               name="email"
-//               value={formData.email}
-//               onChange={handleChange}
-//               required
-//             />
-//             <label htmlFor="email" className="">
-//               Email
-//             </label>
-//           </div>
-
-//           <button className="py-3 rounded-xl bg-[#e31a20] mt-5 w-full  font-poppins text-white text-[16px] md:text-xl ">
-//             SEND REQUEST
-//           </button>
-//         </form>
-//       </div>
-
-//       <div className="lg:hidden py-4 ">
-//         <Services />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default HeroForm;
 import React, { useState } from "react";
-import Services from "./Services";
-import axios from "axios";
-import "./heroform.css";
 import { toast } from "react-toastify";
+import Services from "./Services";
+import "./heroform.css";
 
 const HeroForm = ({ oneline }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  const option = [
+    "Website Design",
+    "Website Development",
+    "Digital Marketing",
+    "PPC",
+    "SEO",
+    "SMO",
+    "Software Development",
+    "Graphic Design",
+    "Mobile App Development",
+    "Offshore Outsourcing",
+  ];
   const [formData, setFormData] = useState({
     name: "",
     phoneNo: "",
+    email: "",
     companyName: "",
     websiteUrl: "",
-    email: "",
+    service: "",
     fromWhere: "dallas",
   });
 
@@ -199,43 +44,46 @@ const HeroForm = ({ oneline }) => {
     const data = {
       name: formData.name,
       company: formData.companyName,
-      website: formData.websiteUrl,
       email: formData.email,
+      website: formData.websiteUrl,
       phone: formData.phoneNo,
+      service: formData.service,
       fromWhere: formData.fromWhere,
     };
 
-    try {
-      const API_TOKEN = "FgRCHG4OVv8Z1BcrjExKJcqspvTsUTCe";
-      const url = "https://www.sibinfotech.com/api/send-email";
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: API_TOKEN,
-        },
-        body: JSON.stringify(data),
-      });
+    console.log(data);
 
-      if (response.status === 200) {
-        toast.success("Email Sent Successfully!");
-        setFormData({
-          name: "",
-          companyName: "",
-          websiteUrl: "",
-          email: "",
-          phoneNo: "",
-        });
+    // try {
+    //   const API_TOKEN = "FgRCHG4OVv8Z1BcrjExKJcqspvTsUTCe";
+    //   const url = "https://www.sibinfotech.com/api/send-email";
+    //   const response = await fetch(url, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: API_TOKEN,
+    //     },
+    //     body: JSON.stringify(data),
+    //   });
 
-        setTimeout(() => {
-          window.location.href = "https://sibinfotech.com/thanks";
-        }, 2000);
-      } else {
-        toast.error("Something went wrong!");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    //   if (response.status === 200) {
+    //     toast.success("Email Sent Successfully!");
+    //     setFormData({
+    //       name: "",
+    //       companyName: "",
+    //       websiteUrl: "",
+    //       email: "",
+    //       phoneNo: "",
+    //     });
+
+    //     setTimeout(() => {
+    //       window.location.href = "https://sibinfotech.com/thanks";
+    //     }, 2000);
+    //   } else {
+    //     toast.error("Something went wrong!");
+    //   }
+    // } catch (error) {
+    //   console.error("Error:", error);
+    // }
   };
 
   return (
@@ -271,6 +119,88 @@ const HeroForm = ({ oneline }) => {
             <div class="user-box mt-[23px] ">
               <input
                 className=""
+                type="text"
+                name="companyName"
+                value={formData.companyName}
+                onChange={handleChange}
+                required
+                // Keyboard type for company name
+                inputMode="text"
+              />
+              <label htmlFor="companyName" className="">
+                Company Name
+              </label>
+            </div>
+          </div>
+          <div className={`grid   ${oneline ? "" : "grid-cols-2 gap-4 "}`}>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
+                type="text"
+                name="websiteUrl"
+                value={formData.websiteUrl}
+                onChange={handleChange}
+                required
+                // Keyboard type for name
+                inputMode="text"
+              />
+              <label htmlFor="websiteUrl" className="">
+                Website Url
+              </label>
+            </div>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
+                type="text"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                // Keyboard type for company name
+                inputMode="text"
+              />
+              <label htmlFor="email" className="">
+                Email
+              </label>
+            </div>
+          </div>
+          {/* <div className={`grid   ${oneline ? "" : "grid-cols-2 gap-4 "}`}>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
+                type="url"
+                name="websiteUrl"
+                value={formData.websiteUrl}
+                onChange={handleChange}
+                required
+                inputMode="url"
+              />
+              <label htmlFor="websiteUrl" className="">
+                Website Url
+              </label>
+            </div>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                // Keyboard type for email
+                inputMode="email"
+              />
+              <label htmlFor="email" className="">
+                Email
+              </label>
+              
+            </div>
+          </div> */}
+
+          <div className={`grid   ${oneline ? "" : "grid-cols-2 gap-4 "}`}>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
                 type="tel"
                 name="phoneNo"
                 value={formData.phoneNo}
@@ -283,56 +213,37 @@ const HeroForm = ({ oneline }) => {
                 Phone No
               </label>
             </div>
+
+            {/* dropdown */}
+            <div className="mt-[23px] text-sm">
+              <select
+                className=" mt-[2px] outline-none w-full relative border-b border-[#9b9b9b] pb-[7px] "
+                name="service"
+                value={formData.service}
+                onChange={handleChange}
+                required
+              >
+                {option.map((option, index) => (
+                  <option className="w-full px-2" key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
-          <div class="user-box mt-[23px] ">
-            <input
-              className=""
-              type="text"
-              name="companyName"
-              value={formData.companyName}
-              onChange={handleChange}
-              required
-              // Keyboard type for company name
-              inputMode="text"
-            />
-            <label htmlFor="companyName" className="">
-              Company Name
-            </label>
+          <div class="relative mb-2 mt-[22px]">
+            {/* <label for="message" class="leading-7 text-sm text-gray-600">
+              Message
+            </label> */}
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Message"
+              class="w-full bg-white text-sm rounded border  border-[#9b9b9b] focus:border-blue-500   h-16 outline-none text-[#9b9b9b] py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out placeholder:text-[#9b9b9b]"
+            ></textarea>
           </div>
 
-          <div class="user-box mt-[23px] ">
-            <input
-              className=""
-              type="url"
-              name="websiteUrl"
-              value={formData.websiteUrl}
-              onChange={handleChange}
-              required
-              // Keyboard type for website URL
-              inputMode="url"
-            />
-            <label htmlFor="websiteUrl" className="">
-              Website Url
-            </label>
-          </div>
-
-          <div class="user-box mt-[23px] ">
-            <input
-              className=""
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-              // Keyboard type for email
-              inputMode="email"
-            />
-            <label htmlFor="email" className="">
-              Email
-            </label>
-          </div>
-
-          <button className="py-3 rounded-xl bg-[#e31a20] mt-5 w-full  font-poppins text-white text-[16px] md:text-xl ">
+          <button className="py-2 rounded-xl bg-[#e31a20]  w-full  font-poppins text-white text-[16px] md:text-xl ">
             SEND REQUEST
           </button>
         </form>
